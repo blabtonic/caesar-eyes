6cd09434a8000dcab618ff9e6f397292f174abee
Trystan Hendricks <dolkhound@gmx.com>
2021-09-14 20:10:07 -0400

    algorithm created

---
+++ src/caesar.rs
@@ -0,0 +1,49 @@
+// Encrypts file with Caesar code
+fn cipher(text: &str, rotation: u8) -> String {
+    let mut res: Vec<char> = vec![' '; text.len()];
+
+    for (idx, letter) in text.chars().enumerate() {
+        res[idx] = if letter <= 'Z' && letter >= 'A' {
+            (((((letter as u8) - ('A' as u8)) + rotation) % 26) + ('A' as u8)) as char
+        } else if letter <= 'z' && letter >= 'a' {
+            (((((letter as u8) - ('a' as u8)) + rotation) % 26) + ('a' as u8)) as char
+        } else {
+            letter
+        };
+    }
+
+    res.into_iter().collect()
+}
+
+pub fn cipher_n(text: &str, rotations: &[u8]) -> Vec<String> {
+    let mut res = Vec::new();
+    for &r in rotations {
+        res.push(cipher(text, r))
+    }
+    res
+}
+
+// Decrypts file from the Caesar code
+fn decipher(text: &str, rotation: u8) -> String {
+    let mut res: Vec<char> = vec![' '; text.len()];
+
+    for (idx, letter) in text.chars().enumerate() {
+        res[idx] = if letter <= 'Z' && letter >= 'A' {
+            (((((letter as u8) - ('A' as u8)) + (26 - rotation)) % 26) + ('A' as u8)) as char
+        } else if letter <= 'z' && letter >= 'a' {
+            (((((letter as u8) - ('a' as u8)) + (26 - rotation)) % 26) + ('a' as u8)) as char
+        } else {
+            letter
+        };
+    }
+
+    res.into_iter().collect()
+}
+
+pub fn decipher_n(text: &str, rotations: &[u8]) -> Vec<String> {
+    let mut res = Vec::new();
+    for &r in rotations {
+        res.push(text, r)
+    }
+    res
+}
