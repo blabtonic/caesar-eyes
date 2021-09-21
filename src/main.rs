3e8d0c6fba9d403b31c75d5cae36b0d15e6f0276
Trystan Hendricks <dolkhound@gmx.com>
2021-09-21 19:54:17 -0400

    cli integration

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use clap::App;
+
+mod caesar;
+mod cli;
+mod io;
+
 fn main() {
-    println!("Hello, world!");
+    let yaml = clap::load_yaml!("cli.yaml");
+    let matches = App::from_yaml(yaml)
+        .version(clap::crate_version!())
+        .author(clap::crate_authors!())
+        .get_matches();
+
+    cli::run(matches);
 }
